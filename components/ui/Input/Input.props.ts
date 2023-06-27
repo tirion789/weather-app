38563d96140faa5f0b2a388e7cdb833af0b97ced
tirion <tirionchen@mail.ru>
2023-06-27 18:22:45 +0300

    button/input components

---
+++ components/ui/Input/Input.props.ts
@@ -0,0 +1,10 @@
+import { ChangeEvent, ReactNode } from 'react';
+export interface InputProps {
+  value: string;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  type?: 'text';
+  inputMod?: 'text' | 'search';
+  placeholder?: string;
+  isDisabled?: boolean;
+  icon?: ReactNode;
+}
