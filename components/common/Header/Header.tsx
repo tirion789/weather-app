7c8160420b822d98a76fac6080e44f9e296d1380
tirion <tirionchen@mail.ru>
2023-06-27 18:30:31 +0300

    input/button

--- components/common/Header/Header.tsx
+++ components/common/Header/Header.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 
 import { Button } from '@ui/Button/Button';
 import { Input } from '@ui/Input/Input';
@@ -11,23 +11,16 @@ import styles from './Header.module.scss';
 
 export const Header = () => {
   const [value, setValue] = useState('');
-  const inputRef = useRef(null);
-  const buttonRef = useRef(null);
-
-  console.log(inputRef.current);
-  console.log(buttonRef.current);
 
   return (
     <div className={styles.container}>
       <Button
-        ref={buttonRef}
         icon={<SunIcon />}
         onClick={() => console.log('asdf')}
         label="Change theme"
         theme="outlined"
       />
       <Input
-        ref={inputRef}
         placeholder={'Enter city name'}
         icon={<IconSearch />}
         value={value}
